4c94359c461ad70da17944d39637849e54a3636f
Deliberate <DESsounds@gmail.com>
2015-01-26 19:48:22 -0800

    google stuff

--- device.mk
+++ device.mk
@@ -171,11 +171,7 @@ PRODUCT_COPY_FILES += \
 	device/moto/shamu/priv-app/TriggerTrainingService/TriggerTrainingService.apk:system/priv-app/TriggerTrainingService/TriggerTrainingService.apk \
 	device/moto/shamu/etc/permissions/com.motorola.triggerenroll.xml:system/etc/permissions/com.motorola.triggerenroll.xml \
 	device/moto/shamu/lib/libtrainingcheck.so:system/lib/libtrainingcheck.so
-<<<<<<< HEAD
 
-=======
-	
->>>>>>> b10790d... Added Ok Google and V4A with drivers
 # Audio effects
 PRODUCT_PACKAGES += \
     libqcomvisualizer \
